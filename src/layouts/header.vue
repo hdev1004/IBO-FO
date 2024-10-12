67fa3008fc7f18a4a6a16bdb7540144ecf87c15c
iam6ukk <kyk001020@naver.com>
2024-10-12 16:45:50 +0900

    add: 헤더 css 추가

--- src/layouts/header.vue
+++ src/layouts/header.vue
@@ -1,7 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AlarmSVG from '@/assets/header/alarm.svg'
+import AlarmActiveSVG from '@/assets/header/alarm_active.svg'
+import ArrowBottomSVG from '@/assets/header/arrow-bottom.svg'
+import ProfileSVG from '@/assets/header/profile.svg'
+</script>
 
 <template>
-  <section>Header</section>
+  <section>
+    <div class="logo">로고</div>
+    <div class="profile">
+      <div class="profile-contents">
+        <span class="profile-name">허광한 님</span>
+        <img :src="ArrowBottomSVG" class="profile-btn" />
+        <img :src="ProfileSVG" class="profile-img" />
+      </div>
+      <img :src="AlarmSVG" class="alram" />
+      <!-- 알람 활성화시, AlarmActiveSVG -->
+    </div>
+  </section>
 </template>
 
 <style lang="scss" scoped>
