c5c98df801e99c07fcbf45c8cc13a85531cbc2b4
iam6ukk <kyk001020@naver.com>
2024-10-13 16:14:58 +0900

    add: 프로필 토글 메뉴 추가, css 수정

--- src/layouts/header.vue
+++ src/layouts/header.vue
@@ -3,21 +3,47 @@ import AlarmSVG from '@/assets/header/alarm.svg'
 import AlarmActiveSVG from '@/assets/header/alarm_active.svg'
 import ArrowBottomSVG from '@/assets/header/arrow-bottom.svg'
 import ProfileSVG from '@/assets/header/profile.svg'
+
+const isMenuOpen = ref(false)
+let currentMenu = ref('')
+
+const toggleMenu = () => {
+  isMenuOpen.value = !isMenuOpen.value
+  // true
+}
+const clickMenu = (type: string) => {
+  currentMenu.value = type
+  isMenuOpen.value = false
+}
 </script>
 
 <template>
-  <section>
-    <div class="header-logo">로고</div>
-    <div class="header-profile">
-      <div class="profile-contents">
-        <span class="profile-name">허광한 님</span>
-        <img :src="ArrowBottomSVG" class="profile-btn" />
-        <img :src="ProfileSVG" class="profile-img" />
+  <header>
+    <div class="header__logo">로고</div>
+    <div class="header__profile">
+      <div class="profile__contents">
+        <p class="profile__contents-name">허광한 님</p>
+        <img
+          :src="ArrowBottomSVG"
+          class="profile__contents-toggleBtn"
+          @click="toggleMenu"
+          :class="{ active: isMenuOpen }"
+        />
       </div>
-      <img :src="AlarmSVG" class="profile-alaram" />
+      <img :src="ProfileSVG" class="header__profile-img" />
+      <img :src="AlarmSVG" class="header__profile-alaram" />
       <!-- 알람 활성화시, AlarmActiveSVG -->
     </div>
-  </section>
+
+    <!-- 드롭다운 메뉴 -->
+    <section class="header__toggleMenu" v-if="isMenuOpen">
+      <ul>
+        <li @click="clickMenu('')">개인정보 변경</li>
+        <!-- 개인정보 변경은 인사기록카드로 이동 -->
+        <li @click="clickMenu('logout')">로그아웃</li>
+      </ul>
+    </section>
+  </header>
 </template>
 
 <style lang="scss" scoped>
