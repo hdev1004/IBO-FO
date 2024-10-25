b170ed7985bd07e99bfa1a7ede4258efabf572ab
iam6ukk <kyk001020@naver.com>
2024-10-25 23:59:16 +0900

    add: 인사정보관리 레이아웃 작업

--- src/pages/home/empInfo.vue
+++ src/pages/home/empInfo.vue
@@ -1,7 +1,29 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import searchSVG from '@/assets/home/search.svg'
+</script>
 
 <template>
-  <section>인사정보</section>
+  <BaseTitle text="인사정보관리" style="margin-top: 80px; margin-left: 80px"></BaseTitle>
+  <main class="empInfo">
+    <section class="empInfo__searchInfo">
+      <div class="empInfo__searchInfo__empStatus">
+        <span>전체 50</span>
+        <div class="v-line"></div>
+        <span>재직 47</span>
+        <div class="v-line"></div>
+        <span>퇴사 3</span>
+      </div>
+      <div class="empInfo__searchInfo__empSearch">
+        <input placeholder="사원명 또는 사번을 입력하세요" />
+        <img :src="searchSVG" class="empInfo__searchInfo__empSearch-searchBtn" />
+      </div>
+
+      <article class="empInfo__searchInfo__empCards"></article>
+    </section>
+    <section class="empInfo_mainInfo"></section>
+  </main>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import url(./empInfo.css);
+</style>
