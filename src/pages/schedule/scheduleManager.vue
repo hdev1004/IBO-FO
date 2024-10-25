d2ee2e1b13824f39b9e15d85cc032e583719a074
hdev1004 <h.dev@kakao.com>
2024-10-26 00:23:52 +0900

    add: tab 컴포넌트 추가

--- src/pages/schedule/scheduleManager.vue
+++ src/pages/schedule/scheduleManager.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
-import BaseTitle from '@/components/title/BaseTitle.vue'
+import BaseTab from '@/components/tab/BaseTab.vue'
+let tabs = ['근무일정', '휴가일정', '테스트', '응애요']
+const tabChange = (value: any) => {
+  console.log(value)
+}
 </script>
 
 <template>
-  <section>
-    <BaseTitle text="인사정보관리"></BaseTitle>
+  <section class="schedule">
+    <BaseTab :tabs="tabs" @tabChange="tabChange"></BaseTab>
+    <div class="schedule-body"></div>
   </section>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import url('./scheduleManager.css');
+</style>
