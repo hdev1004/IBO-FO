f916895599137571f66f42d6f26caf46d4dcaa02
hdev1004 <h.dev@kakao.com>
2024-11-16 00:10:10 +0900

    add: 스케줄 매니저 일정등록 모달 변경

--- src/pages/schedule/tabs/scheduleManagerWorkModal.vue
+++ src/pages/schedule/tabs/scheduleManagerWorkModal.vue
@@ -1,9 +1,107 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const workType = ref(null)
+const workStartDate = ref('')
+const workStartTime = ref('')
+const workEndDate = ref('')
+const workEndTime = ref('')
+const workTitle = ref('')
+const startDisabledDates = (date: any) => {
+  const today = new Date() // 기준일: 오늘
+  today.setDate(today.getDate() - 1)
+
+  // 오늘 이전 날짜를 비활성화하거나 특정일을 비활성화
+  return date < today
+}
+
+const endDisabledDates = (date: any) => {
+  const today = new Date() // 기준일: 오늘
+  today.setDate(today.getDate() - 1)
+
+  // 오늘 이전 날짜를 비활성화하거나 특정일을 비활성화
+  return date < today
+}
+
+const workStartTimeChange = (value: any) => {
+  console.log(value)
+}
+const workEndTimeChange = (value: any) => {
+  console.log(value)
+}
+</script>
 
 <template>
   <section class="schdule-work-background">
     <section class="schedule-work_modal">
       <div class="schedule-work-title">근무 등록</div>
+      <section class="schedule-work-body">
+        <section class="schdule-work-row">
+          <div class="schedule-work-subtitle">근무 구분</div>
+          <div class="schedule-work-radio">
+            <el-radio-group v-model="workType">
+              <el-radio value="Value 1">특근</el-radio>
+              <el-radio value="Value 2">출장</el-radio>
+              <el-radio value="Value 3">직출</el-radio>
+              <el-radio value="Value 4">외근</el-radio>
+            </el-radio-group>
+          </div>
+        </section>
+
+        <section class="schdule-work-row">
+          <div class="schedule-work-subtitle">근무시작</div>
+          <div class="schedule-work-calendar">
+            <el-date-picker
+              style="width: 170px !important"
+              class="schedule-work-date"
+              v-model="workStartDate"
+              type="date"
+              placeholder="근무 시작일"
+              :default-value="new Date()"
+              :disabled-date="startDisabledDates"
+            />
+            <el-time-select
+              style="width: 170px !important"
+              class="schedule-work-time"
+              v-model="workStartTime"
+              start="08:00"
+              step="00:10"
+              end="18:00"
+              @change="workStartTimeChange"
+              format="HH:mm"
+              placeholder="시간을 선택하세요"
+            />
+          </div>
+        </section>
+
+        <section class="schdule-work-row">
+          <div class="schedule-work-subtitle">근무종료</div>
+          <div class="schedule-work-calendar">
+            <el-date-picker
+              style="width: 170px !important"
+              class="schedule-work-date"
+              v-model="workEndDate"
+              type="date"
+              placeholder="근무 시작일"
+              :default-value="new Date()"
+              :disabled-date="endDisabledDates"
+            />
+            <el-time-select
+              style="width: 170px !important"
+              class="schedule-work-time"
+              v-model="workEndTime"
+              start="08:00"
+              step="00:10"
+              end="18:00"
+              format="HH:mm"
+              @change="workEndTimeChange"
+              placeholder="시간을 선택하세요"
+            />
+          </div>
+        </section>
+        <section class="schdule-work-row">
+          <div class="schedule-work-subtitle">제목</div>
+          <input class="schedule-work-input" v-model="workTitle" />
+        </section>
+      </section>
     </section>
   </section>
 </template>
