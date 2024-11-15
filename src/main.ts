6de7da7ef4dc3581b5610017356198e3c04cc281
hdev1004 <h.dev@kakao.com>
2024-11-16 00:09:41 +0900

    fix: element plus 한글 버전 추가

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import ElementPlus from 'element-plus'
 import App from '@/App.vue'
 import router from '@/router/index'
 import 'element-plus/dist/index.css'
+import ko from 'element-plus/es/locale/lang/ko' // 한글 로케일 가져오기
 
 const app = createApp(App)
 
@@ -17,7 +18,7 @@ app.use(pinia)
 /** Vue Router **/
 /** https://router.vuejs.org/ **/
 app.use(router)
-app.use(ElementPlus)
+app.use(ElementPlus, { locale: ko })
 
 /** Unhead **/
 /** https://unhead.unjs.io/ **/
