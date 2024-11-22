88f3370768e4b2251383131dfe7665e038a6f041
hdev1004 <h.dev@kakao.com>
2024-11-23 00:06:43 +0900

    add: 근태관리 추가
    메뉴 router 근태 관리 추가

---
+++ src/router/attendance.ts
@@ -0,0 +1,7 @@
+export const attendanceRouter = [
+  {
+    path: '/attendance/attendanceWork',
+    name: 'attendance-work',
+    component: () => import('@/pages/attendance/attendanceWork.vue')
+  }
+]
