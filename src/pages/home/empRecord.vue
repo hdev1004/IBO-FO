a145d205f4c8d928cd1731441b311d3a99ca5730
iam6ukk <kyk001020@naver.com>
2024-11-24 00:14:59 +0900

    add: 인사기록카드 레이아웃 작업 중

--- src/pages/home/empRecord.vue
+++ src/pages/home/empRecord.vue
@@ -1,13 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ProfileImgSVG from '@/assets/home/empRecord/profileImg.svg'
+import EditProfileImgSVG from '@/assets/home/empRecord/editProfileImg.svg'
+</script>
 
 <template>
   <BaseTitle text="인사기록카드" style="margin-top: 60px; margin-left: 118px"></BaseTitle>
   <main class="container">
     <section class="empRecord__info">
-      <figure class="empRecord__info__img"></figure>
-      <div class="empRecord__info__profile">
-        <div class="empRecord__info__profile"></div>
-        <div class="empRecord__info__icons"></div>
+      <figure class="empRecord__info__profileImg">
+        <img :src="ProfileImgSVG" class="profileImg" />
+        <div class="editProfileImg"><img :src="EditProfileImgSVG" /></div>
+      </figure>
+      <div class="empRecord__info__profileInfo">
+        <div class="empRecord__info__profileInfo__info">
+          <p>허광한</p>
+          <ul>
+            <li>직무 <span class="empRecord__info__profileInfo__info__job">Web Developer</span></li>
+            <li>
+              부서 <span class="empRecord__info__profileInfo__info__dept">Product Service</span>
+            </li>
+            <li>직위 <span class="empRecord__info__profileInfo__info__role">사원</span></li>
+          </ul>
+        </div>
+        <div class="empRecord__info__profileInfo__icons"></div>
       </div>
     </section>
 
