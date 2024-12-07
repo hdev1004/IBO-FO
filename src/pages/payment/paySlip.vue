59a6548b3cd1a9e3c4f2cc4076d1584a4ff8f239
jxxny <axcv__re@naver.com>
2024-12-07 23:58:47 +0900

    add : 급여탭 페이지 추가, 급여대장 진행중

---
+++ src/pages/payment/paySlip.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="container">
+    <p>급여명세서</p>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+@import url(./paySlip.css);
+</style>
