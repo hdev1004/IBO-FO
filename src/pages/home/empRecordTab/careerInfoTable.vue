e3c9c5e34e1e28c69b3ae0409c2ef75cb341fe25
iam6ukk <kyk001020@naver.com>
2024-12-07 23:59:30 +0900

    add: 인사기록카드 탭 메뉴 테이블 추가

---
+++ src/pages/home/empRecordTab/careerInfoTable.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts"></script>
+
+<template>
+  <p>경력·학력</p>
+  <table style="display: flex">
+    <tbody>
+      <tr>
+        <th>이전 근무지</th>
+        <td>20240914P1</td>
+      </tr>
+      <tr>
+        <th>직책</th>
+        <td>010-1234-1234</td>
+      </tr>
+      <tr>
+        <th>근무기간</th>
+        <td>010-1234-1234</td>
+      </tr>
+    </tbody>
+
+    <tbody>
+      <tr>
+        <th>학교명</th>
+        <td>20240914P1</td>
+      </tr>
+      <tr>
+        <th>학사/석사/박사</th>
+        <td>010-1234-1234</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<style lang="scss" scoped>
+@import url('./careerInfoTable.css');
+</style>
