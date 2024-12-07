745d8e68a84d78ae4f2de54890104abfafdb9ba7
hdev1004 <h.dev@kakao.com>
2024-12-07 23:18:13 +0900

    내 신청문서 페이지 추가

---
+++ src/pages/attendance/attendanceApply.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section>내 신청 문서</section>
+</template>
+
+<style lang="scss" scoped>
+@import url('./attendanceApply.css');
+</style>
--- src/router/attendance.ts
+++ src/router/attendance.ts
@@ -3,5 +3,10 @@ export const attendanceRouter = [
     path: '/attendance/attendanceWork',
     name: 'attendance-work',
     component: () => import('@/pages/attendance/attendanceWork.vue')
+  },
+  {
+    path: '/attendance/attendanceApply',
+    name: 'attendance-apply',
+    component: () => import('@/pages/attendance/attendanceApply.vue')
   }
 ]
