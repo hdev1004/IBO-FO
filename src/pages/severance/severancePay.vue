f073e914d023050b7463339d62eff2dc8361c681
iam6ukk <iann6ukk@gmail.com>
2024-12-27 23:55:28 +0900

    add: 퇴직금 산정 완료

--- src/pages/severance/severancePay.vue
+++ src/pages/severance/severancePay.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import ElementPlus from 'element-plus'
+import { QuestionFilled } from '@element-plus/icons-vue'
 import BaseBlueButton from '@/components/buttons/BaseBlueButton.vue'
 import ProfileSVG from '@/assets/home/empInfo/home_searchProfile.svg'
 import TimeSVG from '@/assets/severance/time.svg'
@@ -31,13 +31,13 @@ const date = ref('')
           <div class="content1">
             <div class="content__item">
               <p class="content__title">입사일</p>
-              <p class="content__desc">2024.09.21</p>
+              <p class="content__detail">2024.09.21</p>
             </div>
             <div class="content__item">
               <p class="content__title">퇴사 예정일</p>
               <el-date-picker
                 v-model="date"
-                class="content__desc"
+                class="content__detail"
                 type="date"
                 size="midium"
                 placeholder="일자를 선택하세요"
@@ -45,7 +45,7 @@ const date = ref('')
             </div>
             <div class="content__item">
               <p class="content__title">근무 시간</p>
-              <p class="content__desc">91일</p>
+              <p class="content__detail">91일</p>
               <span class="refer">* 퇴사 예정일 미선택시 오늘날 기준</span>
             </div>
           </div>
@@ -58,27 +58,30 @@ const date = ref('')
           <div class="content2">
             <div class="content__item">
               <p class="content__title">계약 연봉</p>
-              <p class="content__desc">30,000,000원</p>
+              <p class="content__detail">30,000,000원</p>
+              <p class="content__desc">( 월급 : 2,500,000원 )</p>
             </div>
             <div class="content__item">
               <p class="content__title">성과급</p>
-              <p class="content__desc">145,111원</p>
+              <p class="content__detail">145,111원</p>
             </div>
             <div class="content__item">
               <p class="content__title">상여금</p>
-              <p class="content__desc">300,000원</p>
+              <p class="content__detail">300,000원</p>
             </div>
             <div class="content__item">
               <p class="content__title">야근수당</p>
-              <p class="content__desc">0원</p>
+              <p class="content__detail">0원</p>
             </div>
             <div class="content__item">
               <p class="content__title">연장근로수당</p>
-              <p class="content__desc">0원</p>
+              <p class="content__detail">0원</p>
+              <p class="content__desc">연장근로수당은 내규에 의해 지급되지 않습니다.</p>
             </div>
             <div class="content__item">
               <p class="content__title">평균임금금</p>
-              <p class="content__desc">98,170원</p>
+              <p class="content__detail">98,170원</p>
+              <p class="content__desc">( 월급 + 성과급 + 상여금 ) / 일수</p>
             </div>
           </div>
         </el-collapse-item>
@@ -87,14 +90,17 @@ const date = ref('')
             <img :src="DailySVG" class="collaps_icon" />
             1일 통상임금
           </template>
-          <div>Simplify the process: keep operating process simple and intuitive;</div>
-          <div>
-            Definite and clear: enunciate your intentions clearly so that the users can quickly
-            understand and make decisions;
-          </div>
-          <div>
-            Easy to identify: the interface should be straightforward, which helps the users to
-            identify and frees them from memorizing and recalling.
+          <div class="content3">
+            <div class="content__item">
+              <p class="content__title">계약 연봉</p>
+              <p class="content__detail">30,000,000원</p>
+              <p class="content__desc">( 월급 : 2,500,000원)</p>
+            </div>
+            <div class="content__item">
+              <p class="content__title">통상임금</p>
+              <p class="content__detail">82,191원</p>
+              <p class="content__desc">계약 연봉 / 365일</p>
+            </div>
           </div>
         </el-collapse-item>
         <el-collapse-item name="4">
@@ -102,13 +108,46 @@ const date = ref('')
             <img :src="MoneySVG" class="collaps_icon" />
             퇴직소득
           </template>
-          <div>
-            Decision making: giving advices about operations is acceptable, but do not make
-            decisions for the users;
-          </div>
-          <div>
-            Controlled consequences: users should be granted the freedom to operate, including
-            canceling, aborting or terminating current operation.
+          <div class="content4">
+            <div class="content__item">
+              <p class="content__title">퇴직급여액</p>
+              <p class="content__detail">4,417,650원</p>
+              <p class="content__desc">( 퇴직 전 3개월간 평균임금 x 30일 ) x ( 총 근속 연수 )</p>
+            </div>
+            <div class="content__item">
+              <p class="content__title">
+                근속연수공제
+                <el-icon><QuestionFilled /></el-icon>
+              </p>
+              <p class="content__detail">1,500,000원</p>
+            </div>
+            <div class="content__item">
+              <p class="content__title">환산급여</p>
+              <p class="content__detail">23,341,200원</p>
+              <p class="content__desc">( 퇴직소득금액 - 퇴직소득공제 ) x 12 ÷ 근속연수</p>
+            </div>
+            <div class="content__item">
+              <p class="content__title">
+                과세표준
+                <el-icon><QuestionFilled /></el-icon>
+              </p>
+              <p class="content__detail">17,204,720원</p>
+              <p class="content__desc">800만원 + ( 환산급여 - 800만원 ) x 60%</p>
+            </div>
+
+            <div class="content__item">
+              <p class="content__title">
+                환산산출세액
+                <el-icon><QuestionFilled /></el-icon>
+              </p>
+              <p class="content__detail">1,320,708원</p>
+              <p class="content__desc">과세표준 x 세율 - 누진공제액</p>
+            </div>
+            <div class="content__item">
+              <p class="content__title">산출세액</p>
+              <p class="content__detail">1,320,708원</p>
+              <p class="content__desc">환산산출세액 ÷ 12 x 근속연수</p>
+            </div>
           </div>
         </el-collapse-item>
       </el-collapse>
